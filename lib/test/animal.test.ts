1c05a8d173f89d11503658099ec3a780b0a4e332
La-Guillotine <58303108+La-Guillotine@users.noreply.github.com>
2020-12-18 09:59:13 +0100

    Update animal.test.ts

--- lib/test/animal.test.ts
+++ lib/test/animal.test.ts
@@ -1,4 +1,3 @@
-import { Animal } from '../models/animal.model';
 import { expect } from 'chai';
 const chai = require('chai');
 // import * as App from '../index';
@@ -6,38 +5,78 @@ const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
-const API = 'http://localhost:3001'
+const API = 'http://localhost:3001';
 
 describe('Animal', () => {
     describe('#getAll', () => {
-        it('should return all animals',() => {
+        it('should return all animals',(done) => {
             chai
             .request(API)
             .get('/animals')
             .end((err, res) => {
                 expect(res.status).to.equals(200);
-                expect(res.body).to.have.length(18);
+                expect(res.body).to.be.an('array');
+                expect(res.body[1].name).to.equals('Chien');
+                done();
             });
         });
     });
 
     describe('#getOne', () => {
-        it('should return one animal', async () => {
-            let animal = (await Animal.findOne({
-                where: { id: 1 }
-            })).toJSON();
+        it('should return one animal', (done) => {
+            chai
+            .request(API)
+            .get('/animals/1')
+            .end((err, res) => {
+                expect(res.status).to.equals(200);
+                expect(res.body).to.be.an('object');
+                expect(res.body.id).to.equals(1);
+                expect(res.body.name).to.equals('Chat');
+                done();
+            });
+        });
+    });
 
-            expect(animal).not.null;
-            expect(animal).to.be.an('object');
+    describe('#Add', () => {
+        it('should add one animal on database', (done) => {
+            chai
+            .request(API)
+            .post('/animals')
+            .send({name: 'Dragon'})
+            .end((err, res) => {
+                expect(res.status).to.equals(200);
+                expect(res.body).to.be.an('object');
+                expect(res.body.name).to.equals('Dragon');
+                done();
+            });
+        });
+    });
+
+    describe('#UpdateOne', () => {
+        it('should update one animal', (done) => {
+            chai
+            .request(API)
+            .patch('/animals/19')
+            .send({name: 'Couleuvre'})
+            .end((err, res) => {
+                expect(res.status).to.equals(200);
+                expect(res.body).to.be.an('object');
+                expect(res.body.id).to.equals(19);
+                expect(res.body.name).to.equals('Couleuvre');
+                done();
+            });
+        });
+    });
+
+    describe('#DeleteOne', () => {
+        it('should delete one animal', (done) => {
+            chai
+            .request(API)
+            .delete('/animals/19')
+            .end((err, res) => {
+                expect(res.status).to.equals(200);
+                done();
+            });
         });
     });
 });
-
-// var assert = require('assert');
-// describe('Array', function() {
-//   describe('#indexOf()', function() {
-//     it('should return -1 when the value is not present', function() {
-//       assert.equal([1, 2, 3].indexOf(4), -1);
-//     });
-//   });
-// });

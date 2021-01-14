8ad9a8ca367f7382fc28ca886369f37d16424caf
La-Guillotine <58303108+La-Guillotine@users.noreply.github.com>
2021-01-14 09:09:28 +0100

    Update database.ts

--- lib/config/database.ts
+++ lib/config/database.ts
@@ -2,4 +2,8 @@
 import { Sequelize } from "sequelize";
 require('dotenv').config()
 
-export const database = new Sequelize(process.env.DB_URL);
+// export const database = new Sequelize(process.env.DB_URL);
+export const database = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  });

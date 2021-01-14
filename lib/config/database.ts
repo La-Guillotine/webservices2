36b5bff9c8aa7394a3a19c4dce7a9fc8a23f68cf
La-Guillotine <58303108+La-Guillotine@users.noreply.github.com>
2021-01-14 09:42:01 +0100

    test mysqlshow fixtures

--- lib/config/database.ts
+++ lib/config/database.ts
@@ -2,7 +2,6 @@
 import { Sequelize } from "sequelize";
 require('dotenv').config()
 
-console.log(process.env.DB_PASSWORD);
 export const database = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql'
